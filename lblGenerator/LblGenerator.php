4928ba70e3d6c5d7431b08281fcf24ad85fbbd58
ale.cristoforetti <ale.cristoforetti@AULAS51-PC20.itib.local>
2022-11-22 12:45:53 +0100

    lblGen. fix 22/11

--- lblGenerator/LblGenerator.php
+++ lblGenerator/LblGenerator.php
@@ -1,6 +1,18 @@
 <html>
     <head>
-
+        <div class="bd-example-snippet bd-code-snippet"><div class="bd-example">
+        <nav aria-label="Standard pagination example">
+          <ul class="pagination">
+            <li class="page-item">
+            </li>
+            <li class="page-item"><a class="page-link" href="index.html">1</a></li>
+            <li class="page-item"><a class="page-link" href="LblGenerator.php">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item">
+            </li>
+          </ul>
+        </nav>
+        </div></div>
     </head>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
