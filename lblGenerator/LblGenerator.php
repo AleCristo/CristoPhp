c3c74acda611ac431088c9ca896933408a1f43db
ale.cristoforetti <ale.cristoforetti@AULAS51-PC20.itib.local>
2022-11-22 12:15:57 +0100

    lblGen. fix 22/11

--- lblGenerator/LblGenerator.php
+++ lblGenerator/LblGenerator.php
@@ -1,41 +1,34 @@
 <html>
     <head>
-        <style>
-            body{
-                align-items: center;
-            }
-            table
-            {
-                border-style:solid;
-                border-width:2px;
-                border-color:black;
-            }
-        </style>
+
     </head>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    
     <body>
-        
+
         <br>
         <?php
         $title = 'Test LblGenerator';
-        
+
         $rows = 8;
         $colums = 10;
         $r = 0;
         $c = 0;
-        
-        echo "<table border='2'>
+
+        echo "<table class='table table-bordered table-hover table-responsive'>
             <tr> <th colspan=" . $colums . ">" . $title . "</th></tr>";
 
-        for($i = 0; $i < $colums; $i++){
+        for ($i = 0; $i < $colums; $i++) {
             echo "<tr>";
-            for($j = 0; $j < $rows; $j++){
+            for ($j = 0; $j < $rows; $j++) {
                 echo"<td>" . $c . $r . "</td>";
                 $r++;
             }
-            $r=0;
+            $r = 0;
             $c++;
-        } 
+        }
 
         echo "</table>";
         ?>
