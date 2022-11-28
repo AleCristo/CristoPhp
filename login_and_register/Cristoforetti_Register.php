4274cc3bc623730c72832b1ca0fbdf457ef43076
alecr <alecr@DESKTOP-4CHG8JO.homenet.telecomitalia.it>
2022-11-28 19:33:28 +0100

    added login_and_register

---
+++ login_and_register/Cristoforetti_Register.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+Your firstName is: <?php echo $_POST["firstName"]; ?>
+Your lastName is: <?php echo $_POST["lastName"]; ?>
+Your username is: <?php echo $_POST["username"]; ?>
+Your email address is: <?php echo $_POST["gmail"]; ?>
+Your address is: <?php echo $_POST["address"]; ?>
+Your password is: <?php echo $_POST["password"]; ?>
+Your number is: <?php echo $_POST["number"]; ?>
+Your fiscal code is: <?php echo $_POST["cf"]; ?>
+Your birth date is: <?php echo $_POST["date"]; ?>
+Your country is: <?php echo $_POST["country"]; ?>
+Your state is: <?php echo $_POST["state"]; ?>
+
+</body>
+</html>
